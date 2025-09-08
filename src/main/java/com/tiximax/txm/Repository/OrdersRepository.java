35d47420edc63901d18d5d2208d4f95ef0752ce4
Phat <phatbt237@gmail.com>
2025-09-08 09:30:07 +0700

    fix application

--- src/main/java/com/tiximax/txm/Repository/OrdersRepository.java
+++ src/main/java/com/tiximax/txm/Repository/OrdersRepository.java
@@ -4,14 +4,12 @@ import com.tiximax.txm.Entity.Orders;
 import com.tiximax.txm.Enums.OrderStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 @Repository
